322e48cd4ac5f90fe7a54378d800fd50baad867d
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-12 10:00:04 +0100

    add require for spec helper

---
+++ spec/genre_spec.rb
@@ -0,0 +1,18 @@
+require_relative ('spec_helper')
+
+# describe Genre do
+#     let(:genre) { Genre.new('Rock') }
+    
+#     it 'is an instance of Genre' do
+#         expect(genre).to be_an_instance_of Genre
+#     end
+    
+#     it 'has a name' do
+#         expect(genre.name).to eq 'Rock'
+#     end
+    
+#     it 'has an id' do
+#         expect(genre.id).to_not be_nil
+#     end
+    
+# end
