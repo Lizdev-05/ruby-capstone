921aba117953e8db098697f966f610fd5d8c8481
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-10-12 10:00:44 +0100

    test the music

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,18 +1,18 @@
-# require_relative ('spec_helper')
+require_relative ('spec_helper')
 
-# describe Music do
-#     let(:music) { Music.new('2021-01-01', true) }
+describe Music do
+    let(:music) { Music.new('2021-01-01', true) }
     
-#     it 'is an instance of Music' do
-#         expect(music).to be_an_instance_of Music
-#     end
+    it 'is an instance of Music' do
+        expect(music).to be_an_instance_of Music
+    end
     
-#     it 'has a publish date' do
-#         expect(music.publish_date).to eq '2021-01-01'
-#     end
+    it 'has a publish date' do
+        expect(music.publish_date).to eq '2021-01-01'
+    end
     
-#     it 'has a sportify value' do
-#         expect(music.on_sportify).to eq true
-#     end
+    it 'has a sportify value' do
+        expect(music.on_sportify).to eq true
+    end
     
-#  end
+ end
