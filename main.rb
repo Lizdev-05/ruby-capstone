ee6034a4586230411248243cec95f439293f45be
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-10-13 18:09:57 +0100

    Fix linters erorrs

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
+require './app.rb'
 
+APP = Application.new
+
+class Main
   def menu
     puts 'Welcome to the Catalog of many things console application'
     puts "
@@ -9,77 +13,86 @@
     4 - Add a game
     5 - List all music albums
     6 - Add a music album
-    5 - Add a music album
-    6 - List all music albums
     7 - List all genres
     8 - List all labels
     9 - List all authors
     10 - Exit "
   end
 
-  def prompt
-    menu
-    choice = gets.chomp.to_i
-    options(choice)
-    prompt until @choice == 10
+  def run
+    loop do
+      menu
+      choice = gets.chomp.to_i
+      options(choice)
+      run until @choice == 10
+    end
   end
 
   def options(choice)
+    book_menu(choice)
     game_menu(choice)
-    # when 1
-    #   @app.list_all_books
-    # when 2
-    #   @app.add_book
-    # when 3
-    #   @app.list_all_games
-    # when 4
-    #   @app.add_game
-    # when 5
-    #   @app.list_all_music_albums
-    # when 6
-    #   @app.add_music_album
-    # when 7
-    #   @app.list_all_genres
-    # when 8
-    #   @app.list_all_labels
-    # when 9
-    #   @app.list_all_authors
-    # else
-    #   exit
-    # end
+    music_menu(choice)
+    genre_menu(choice)
+    label_menu(choice)
+    author_menu(choice)
+    exit_menu(choice)
   end
 
-  def game_menu(choice)
+  def book_menu(choice)
     case choice
     when 1
       APP.list_all_books
     when 2
       APP.add_book
+    end
+  end
+
+  def game_menu(choice)
+    case choice
     when 3
       APP.list_all_games
     when 4
       APP.add_game
+    end
+  end
+
+  def music_menu(choice)
+    case choice
     when 5
       APP.list_all_music_albums
     when 6
       APP.add_music_album
+    end
+  end
+
+  def genre_menu(choice)
+    case choice
     when 7
       APP.list_all_genres
+    end
+  end
+
+  def label_menu(choice)
+    case choice
     when 8
       APP.list_all_labels
+    end
+  end
+
+  def author_menu(choice)
+    case choice
     when 9
       APP.list_all_authors
-    when 5
-      @app.add_music_album
-    when 6
-      @app.list_all_music_albums
-    when 7
-      @app.list_all_genres
-    else
+    end
+  end
+
+  def exit_menu(choice)
+    case choice
+    when 10
       exit
     end
   end
 end
 
 app = Main.new
-app.prompt
+app.run
