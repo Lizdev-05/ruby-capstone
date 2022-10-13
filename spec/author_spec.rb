23de3381b386788f0586debb71215f0d4a35f635
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-10-13 18:12:39 +0100

    fix linters errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,28 +1,21 @@
 require 'rspec'
 require_relative '../classes/author'
 
-
 describe 'Author' do
-    before :each do
-      @test_author = Author.new('First Name', 'Last Name')
-      @test_item = Game.new(true, '2001-01-01', '2000-01-01')
-    end
-    
-    it 'should be an instance of Author' do
-      expect(@test_author).to be_an_instance_of(Author)
-    end
+  before :each do
+    @test_author = Author.new('First Name', 'Last Name')
+    @test_item = Game.new(true, '2001-01-01', '2000-01-01')
+  end
 
-    it 'should have a first name' do
-        expect(@test_author.first_name).to eq('First Name')
-        end
+  it 'should be an instance of Author' do
+    expect(@test_author).to be_an_instance_of(Author)
+  end
 
-    it 'should have a last name' do
-        expect(@test_author.last_name).to eq('Last Name')
-    end
+  it 'should have a first name' do
+    expect(@test_author.first_name).to eq('First Name')
+  end
 
+  it 'should have a last name' do
+    expect(@test_author.last_name).to eq('Last Name')
+  end
 end
-
-    
-
-
-  
