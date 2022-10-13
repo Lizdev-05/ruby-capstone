8fddd6023166f6ae094eaaacb384be0ace366693
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-13 14:19:28 +0100

    Update: game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe 'Game class' do
       expect(@game.multiplayer).to eq true
     end
 
-    it 'should return correct value for game last played date' do
+    it 'should return correct value for last played date' do
       expect(@game.last_played_at.to_s).to eq '2022-10-12'
     end
 
-    it 'should return correct value for game publish date' do
+    it 'should return correct value for publish date' do
       expect(@game.publish_date.to_s).to eq '2022-12-12'
     end
   end
